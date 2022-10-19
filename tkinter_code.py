df6c2fc69d7ebc1a704442942a1b5b8180a95424
RyzenMu <muruga13in2022@gmail.com>
2022-10-19 22:01:13 +0530

    buttons completed

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,7 +1,23 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
 
 frame = Tk()
+frame.title('Simple Calculator')
+
+# making integer variables
+number_1 = IntVar()
+number_2 = IntVar()
+number_3 = IntVar()
+number_4 = IntVar()
+number_5 = IntVar()
+number_6 = IntVar()
+number_7 = IntVar()
+number_8 = IntVar()
+number_9 = IntVar()
+number_0 = IntVar()
+
+def submit():
+    print(1)
 
 frame = Frame(frame, bg='blue', width=500, height=500)
 frame.grid(row=0, column=0)
@@ -9,10 +25,10 @@ frame.grid(row=0, column=0)
 entry = Entry(frame, width=40)
 entry.grid(row=1, column=(1), columnspan=8)
 
-button_1 = Button(frame, text='1', fg='black', width=10)
+button_1 = Button(frame, text='1', fg='black', width=10, textvariable=number_1)
 button_1.grid(row = 2, column=0)
 
-button_2 = Button(frame, text='2', fg='black', width=10)
+button_2 = Button(frame, text='2', fg='black', width=10, textvariable= number_2)
 button_2.grid(row = 2, column=1)
 
 button_3 = Button(frame, text='3', fg='black', width=10)
@@ -48,7 +64,15 @@ button_multiply.grid(row = 4, column=3)
 button_divide = Button(frame, text='/', fg='black', width=10)
 button_divide.grid(row = 5, column=0)
 
-button_equal = Button(frame, text='=', fg='black', width=10)
-button_equal.grid(row = 5, column=1, columnspan=6)
+button_equal = Button(frame, text='=', fg='black', width=10, command=submit)
+button_equal.grid(row = 5, column=1)
+
+button_zero = Button(frame, text='0', fg='black', width=10)
+button_zero.grid(row = 5, column=2)
+
+
+
+
+
 
 frame.mainloop()
