de3f733e1ad21340c1c22ffa2089fc763b341319
RyzenMu <muruga13in2022@gmail.com>
2022-10-21 20:57:01 +0530

    value added to entry box

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,34 +1,29 @@
 import tkinter as tk
 from tkinter import *
+from turtle import onclick
+from more_itertools import one
+from tkinter import ttk
+
+from numpy import number
 
 frame = Tk()
 frame.title('Simple Calculator')
+frame.geometry('700x500')
 
-# making integer variables
-number_1 = IntVar()
-number_2 = IntVar()
-number_3 = IntVar()
-number_4 = IntVar()
-number_5 = IntVar()
-number_6 = IntVar()
-number_7 = IntVar()
-number_8 = IntVar()
-number_9 = IntVar()
-number_0 = IntVar()
+def func(args):
+    entry.insert(0, args)
 
-def submit():
-    print(1)
 
 frame = Frame(frame, bg='blue', width=500, height=500)
 frame.grid(row=0, column=0)
 
-entry = Entry(frame, width=40)
+entry = Entry(frame, width=40, relief=RIDGE)
 entry.grid(row=1, column=(1), columnspan=8)
 
-button_1 = Button(frame, text='1', fg='black', width=10)
+button_1 = Button(frame, text='1', fg='black', width=10, command=lambda: func(1))
 button_1.grid(row = 2, column=0)
 
-button_2 = Button(frame, text='2', fg='black', width=10)
+button_2 = Button(frame, text='2', fg='black', width=10, command = lambda: func(2))
 button_2.grid(row = 2, column=1)
 
 button_3 = Button(frame, text='3', fg='black', width=10)
@@ -64,15 +59,29 @@ button_multiply.grid(row = 4, column=3)
 button_divide = Button(frame, text='/', fg='black', width=10)
 button_divide.grid(row = 5, column=0)
 
-button_equal = Button(frame, text='=', fg='black', width=10, command=submit)
+button_equal = Button(frame, text='=', fg='black', width=10)
 button_equal.grid(row = 5, column=1)
 
-button_zero = Button(frame, text='0', fg='black', width=10)
-button_zero.grid(row = 5, column=2)
+button_0 = Button(frame, text='0', fg='black', width=10)
+button_0.grid(row = 5, column=2)
 
 
+# making integer variables
+number_1 = IntVar(button_1, value=1, name='1')
+number_2 = IntVar(button_2, value=2, name='2')
+number_3 = IntVar(button_3, value=3, name='3')
+number_4 = IntVar(button_4, value=4, name='4')
+number_5 = IntVar(button_5, value=5, name='5')
+number_6 = IntVar(button_6, value=6, name='6')
+number_7 = IntVar(button_7, value=7, name='7')
+number_8 = IntVar(button_8, value=8, name='8')
+number_9 = IntVar(button_9, value=9, name='9')
+number_0 = IntVar(button_0, value=0, name='0')
 
 
 
 
 frame.mainloop()
+
+
+print(number_0)
