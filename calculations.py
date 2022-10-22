2d88e6dab04f2c97772c274b234912a6776a21b5
RyzenMu <muruga13in2022@gmail.com>
2022-10-22 20:27:46 +0530

    tkinter addition not working

--- calculations.py
+++ calculations.py
@@ -1,2 +1,4 @@
-from tkinter_code import *
+
+
+
 
--- tkinter_code.py
+++ tkinter_code.py
@@ -1,17 +1,27 @@
-import tkinter as tk
-from tkinter import *
-from turtle import onclick
-from more_itertools import one
-from tkinter import ttk
 
-from numpy import number
+from tkinter import *
 
 frame = Tk()
 frame.title('Simple Calculator')
 frame.geometry('700x500')
 
+first_number = 0
+
 def func(args):
-    entry.insert(0, args)
+    # entry.insert(0, args)
+    entry.insert(END, args)
+
+
+def clear():
+    # entry.set = ""
+    entry.delete(0, END)
+
+def add():
+    first_number = int(entry.get())
+    entry.delete(0, END)
+    print(first_number)
+    
+
 
 
 frame = Frame(frame, bg='blue', width=500, height=500)
@@ -26,45 +36,34 @@ button_1.grid(row = 2, column=0)
 button_2 = Button(frame, text='2', fg='black', width=10, command = lambda: func(2))
 button_2.grid(row = 2, column=1)
 
-button_3 = Button(frame, text='3', fg='black', width=10)
+button_3 = Button(frame, text='3', fg='black', width=10, command = lambda : func(3))
 button_3.grid(row = 2, column=2)
 
-button_plus = Button(frame, text='+', fg='black', width=10)
-button_plus.grid(row = 2, column=3)
 
-button_4 = Button(frame, text='4', fg='black', width=10)
+
+button_4 = Button(frame, text='4', fg='black', width=10, command=lambda: func(4))
 button_4.grid(row = 3, column=0)
 
-button_5 = Button(frame, text='5', fg='black', width=10)
+button_5 = Button(frame, text='5', fg='black', width=10, command = lambda : func(5))
 button_5.grid(row = 3, column=1)
 
-button_6 = Button(frame, text='6', fg='black', width=10)
+button_6 = Button(frame, text='6', fg='black', width=10, command = lambda : func(6))
 button_6.grid(row = 3, column=2)
 
-button_minus = Button(frame, text='-', fg='black', width=10)
-button_minus.grid(row = 3, column=3)
 
-button_7 = Button(frame, text='7', fg='black', width=10)
+
+button_7 = Button(frame, text='7', fg='black', width=10, command=lambda: func(7))
 button_7.grid(row = 4, column=0)
 
-button_8 = Button(frame, text='8', fg='black', width=10)
+button_8 = Button(frame, text='8', fg='black', width=10, command= lambda: func(8))
 button_8.grid(row = 4, column=1)
 
-button_9 = Button(frame, text='9', fg='black', width=10)
+button_9 = Button(frame, text='9', fg='black', width=10, command= lambda: func(9))
 button_9.grid(row = 4, column=2)
 
-button_multiply = Button(frame, text='*', fg='black', width=10)
-button_multiply.grid(row = 4, column=3)
 
-button_divide = Button(frame, text='/', fg='black', width=10)
-button_divide.grid(row = 5, column=0)
-
-button_equal = Button(frame, text='=', fg='black', width=10)
-button_equal.grid(row = 5, column=1)
-
-button_0 = Button(frame, text='0', fg='black', width=10)
+button_0 = Button(frame, text='0', fg='black', width=10, command= lambda: func(0))
 button_0.grid(row = 5, column=2)
-
 
 # making integer variables
 number_1 = IntVar(button_1, value=1, name='1')
@@ -79,9 +78,27 @@ number_9 = IntVar(button_9, value=9, name='9')
 number_0 = IntVar(button_0, value=0, name='0')
 
 
+button_plus = Button(frame, text='+', fg='black', width=10, command = lambda : add())
+button_plus.grid(row = 2, column=3)
 
+button_minus = Button(frame, text='-', fg='black', width=10)
+button_minus.grid(row = 3, column=3)
+
+button_multiply = Button(frame, text='*', fg='black', width=10)
+button_multiply.grid(row = 4, column=3)
+
+button_divide = Button(frame, text='/', fg='black', width=10)
+button_divide.grid(row = 5, column=0)
+
+button_equal = Button(frame, text='=', fg='black', width=10)
+button_equal.grid(row = 5, column=1)
+
+button_c = Button(frame, text='C', fg='black', width=10, command = lambda : clear())
+button_c.grid(row=5, column=3)
 
 frame.mainloop()
 
 
-print(number_0)
+
+
+
